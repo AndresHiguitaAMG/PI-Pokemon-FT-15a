04d8fd43aa00f9c382ab9927af985703259427ce
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2022-01-14 23:46:26 -0500

    new modification

--- api/src/controllers/pokemonsController.js
+++ api/src/controllers/pokemonsController.js
@@ -1,115 +1,82 @@
-const { Pokemon, Type } = require('../db');
+const { Pokemon, Type, Op } = require('../db');
 const axios = require('axios');
+// const answerbyName = require('../assistant/index');
 
 //#region función para buscar por query o toda la información 
-const getPokemons = async (req, res) => {
-    try {
-        let {
-            name,
-            order
-        } = req.query;
-
-        let informationAPI;
-        let informationDataBase;
-        let allData = [];   
-
-        if (name && name !== "") {
-            informationDataBase = await Pokemon.findOne({
-                include: {
-                    model: Type,
-                    attributes: ["name"],
-                    through: {
-                        attributes: []
-                    } 
+const getPokemons = async (req, res, next) => {
+    const name = req.query.name;
+    if (name) {
+        try {
+            const InformationNameDataBase = await Pokemon.findOne({
+                where: {
+                    name: {
+                        [Op.iLike]: `%${name}%`,
+                    },
                 },
-                where:{
-                    name
-                }
-            });
-            const byNameAPI = !informationDataBase ? (await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)).data : false;
-            if (informationDataBase !== null) {
-                informationDataBase = informationDataBase.dataValues;
-                // informationDataBase = informationDataBase.map(e => {
-                //     return {
-                //         name: e.name,
-                //         types: e.types,
-                //         id: e.id,
-                //         createdInDatabase: e.createdInDatabase
-                //     }
-                // });
-            } 
-            informationAPI = byNameAPI && {
-                image: byNameAPI.sprites.front_shiny,
-                name: byNameAPI.name,
-                types: byNameAPI.types[0].type.name,
-                id: byNameAPI.id,
-            }
-            allData = byNameAPI ? informationAPI : informationDataBase;
-        } else {
-            const myPokemonsOnPageOne = (await axios.get("https://pokeapi.co/api/v2/pokemon")).data.results;
-            const myInformationOne = myPokemonsOnPageOne.map(el => axios.get(el.url));
-            const myPokemonsOnPageTwo = (await axios.get("https://pokeapi.co/api/v2/pokemon")).data;
-            const open = await axios.get(myPokemonsOnPageTwo.next);
-            const nextPage = open.data.results;
-            const myInformationTwo = nextPage.map(e => axios.get(e.url));
-            const myInformation = myInformationOne.concat(myInformationTwo);
-            const myObject = await Promise.all(myInformation);
-            informationAPI = myObject.map(e => {
-                return {
-                    image: e.data.sprites.front_shiny,
-                    name: e.data.name,
-                    types: e.data.types[0].type.name,
-                    id: e.data.id
-                }
-            });
-            informationDataBase = await Pokemon.findAll({
                 include: {
                     model: Type,
                     attributes: ["name"],
                     through: {
                         attributes: []
                     }
-                 }
-                });
-                informationDataBase = informationDataBase.map(el => {
-                    return {
-                    // image: el.image,
-                    name: el.name,
-                    types: el.types,
-                    id: el.id,
-                    createdInDatabase: el.createdInDatabase
                 }
             });
-            allData = informationDataBase.concat(informationAPI);
-        }
-        //#region order
-        if (Array.isArray(allData)) {
-            if (order === "asc" || !order || order === "") {
-                allData = allData.sort((a, b) => {
-                    return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
-                }); 
+              
+            if (InformationNameDataBase !== null) {
+                res.json(InformationNameDataBase);
             } else {
-                allData = allData.sort((a, b) => {
-                    return b.name.toLowerCase().localeCompare(a.name.toLowerCase());
-                });
+                const byNameAPI = (await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)).data;
+                const informationNameAPI = {
+                    image: byNameAPI.sprites.front_shiny,
+                    name: byNameAPI.name,
+                    types: byNameAPI.types[0].type.name,
+                    id: byNameAPI.id
             }
-        }
-        //#endregion
-        if (allData.length === 0) {
-            res.status(400).json({message: "Not Found"})
-        } else {
-            return res.json(allData);
+            res.json(informationNameAPI); 
         }
     } catch (error) {
-        if (error.response !== undefined) {
-            console.log(error.response.data);
-            return res.status(400).send({message: "Pokemón not found"});
-        } else {
-            console.log(error);
-            return res.status(400).send({message: "unexpected error"});
+        return res.status(404).send({message: "Pokemón not found"});
         }
+    } else {
+        let informationDataBase = await Pokemon.findAll({
+            include: {
+                model: Type,
+                attributes: ["name"],
+                through: {
+                    attributes: []
+                }
+            }
+        });
+        informationDataBase = informationDataBase.map(el => {
+            return {
+                name: el.name,
+                types: el.types,
+                id: el.id,
+                createdInDatabase: el.createdInDatabase
+            }
+        });
+        const myPokemonsOnPageOne = (await axios.get("https://pokeapi.co/api/v2/pokemon")).data.results;
+        const myInformationOne = myPokemonsOnPageOne.map(el => axios.get(el.url));
+        const myPokemonsOnPageTwo = (await axios.get("https://pokeapi.co/api/v2/pokemon")).data;
+        const open = await axios.get(myPokemonsOnPageTwo.next);
+        const nextPage = open.data.results;
+        const myInformationTwo = nextPage.map(e => axios.get(e.url));
+        let myInformation = myInformationOne.concat(myInformationTwo);
+        const myObject = await Promise.all(myInformation);
+            const informationAPI = myObject.map(e => {
+                return {
+                    image: e.data.sprites.front_shiny,
+                    name: e.data.name,
+                    types: e.data.types[0].type.name,
+                    id: e.data.id
+               }
+           });
+           const allPokemons = informationAPI.concat(informationDataBase)
+           return res.json(allPokemons);
     }
 }
+
+
 //#endregion
 
 //#region Función para id
